31faf887b04a9913f6a93603aa42c9457cd02388
Eric Schles <ericschles@gmail.com>
2015-07-30 09:00:55 -0400

    things

--- app.py
+++ app.py
@@ -1,25 +1,18 @@
 from flask import Flask, request, redirect
-import twilio.twiml 
-import datetime
-import os
-
+import twilio.twiml
+ 
 app = Flask(__name__)
-
-
+ 
+# Try adding your own number to this li
+ 
 @app.route("/", methods=['GET', 'POST'])
 def hello_monkey():
     # Get the caller's phone number from the incoming Twilio request
     from_number = request.values.get('From', None)
     resp = twilio.twiml.Response()
-    print from_number
-    # if the caller is someone we know:
-    if from_number in callers:
-        # Greet the caller by name
-        resp.say("Hello " + callers[from_number])
-    else:
-        resp.say("Hello Monkey")
+    resp.say("Hello Monkey")
  
-    return str(resp)
+    return str(resp),from_number
  
 if __name__ == "__main__":
     app.run(debug=True)

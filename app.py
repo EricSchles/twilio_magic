fd47238f76fdf93e6b7e1311e7d4ac6671d50dc7
Eric Schles <ericschles@gmail.com>
2015-07-30 08:52:14 -0400

    things

--- app.py
+++ app.py
@@ -1,33 +1,34 @@
 from flask import Flask, request, redirect
 import twilio.twiml
-from flask.ext.sqlalchemy import SQLAlchemy 
+#from flask.ext.sqlalchemy import SQLAlchemy 
 import datetime
 import os
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ['DATABASE_URL']
-db = SQLAlchemy(app)
+#app.config["SQLALCHEMY_DATABASE_URI"] = os.environ['DATABASE_URL']
+#db = SQLAlchemy(app)
 
 # Try adding your own number to this list!
-class Logger(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    caller = db.Column(db.String(400))
-    timestamp = db.Column(db.DateTime, default=datetime.datetime.now)
+# class Logger(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     caller = db.Column(db.String(400))
+#     timestamp = db.Column(db.DateTime, default=datetime.datetime.now)
 
-    def __init__(self,caller):
-        self.caller = caller
+#     def __init__(self,caller):
+#         self.caller = caller
 
-    def __repr__(self):
-        return '<ip_addr %r>' % self.ip_address
+#     def __repr__(self):
+#         return '<ip_addr %r>' % self.ip_address
 
 @app.route("/", methods=['GET', 'POST'])
 def hello_monkey():
     # Get the caller's phone number from the incoming Twilio request
     from_number = request.values.get('From', None)
     resp = twilio.twiml.Response()
-    call = Logger(from_number)
-    db.session.add(call)
-    db.session.commit()
+    print from_number
+    #call = Logger(from_number)
+    #db.session.add(call)
+    #db.session.commit()
     # if the caller is someone we know:
     if from_number in callers:
         # Greet the caller by name
